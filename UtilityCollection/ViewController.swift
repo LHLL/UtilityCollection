aef998f5eecc4eb4336336b3ee56c1a109786568
Lynn Xu <LynnHui819@gmail.com>
2017-08-15 18:57:57 -0700

    Initial Commit

---
+++ UtilityCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UtilityCollection
+//
+//  Created by 李玲 on 8/15/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
